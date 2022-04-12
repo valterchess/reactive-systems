c8c6af3d852b700a3968d7d858fbe0c02aeac605
valterchess <valterrekabano7@gmail.com>
2022-04-12 19:13:46 -0300

    add new comments

--- actor-hello-world/src/main/java/com/lightbend/akka/sample/HelloWorld.java
+++ actor-hello-world/src/main/java/com/lightbend/akka/sample/HelloWorld.java
@@ -10,12 +10,13 @@ import akka.actor.typed.javadsl.Receive;
 // Determina que a classe terá um comportamento de ACTOR
 // O tipo que AbstractBehavior recebe determina o tipo especifico
 // de mesagem que o Actor pode receber
-public class HelloWorld extends AbstractBehavior<HelloWorld.Command>{
+public class HelloWorld extends AbstractBehavior<HelloWorld.Command> {
 
 
     // A melhor prática para definir o tipo das mensagens que o ACTOR irá aceitar
     // É definir uma nested marker interface como o nível superior das mensagens
-    interface Command {}
+    interface Command {
+    }
 
     // Aqui definiremos os dois tipos de mensagens concretas
     // que o Actor pode receber
@@ -28,28 +29,43 @@ public class HelloWorld extends AbstractBehavior<HelloWorld.Command>{
         INSTANCE
     }
 
+    //Como essa mensagem tem um parametro que deverá ser a nova msg hello
+    // então foi definida como uma classe mesmo
     public static class ChangeMessage implements Command {
+        // Como todas as mensagens no Akka devem ser imutáveis
+        // podemos pular os getters e setters e definir os campos como public final
         public final String newMessage;
 
+        //Definimos então um construtor que receberá o parâmetro
         public ChangeMessage(String newMessage) {
             this.newMessage = newMessage;
         }
     }
 
-    public static Behavior<Command> create(){
+
+    public static Behavior<Command> create() {
         return Behaviors.setup(HelloWorld::new);
     }
 
+    // Aqui é definido o estado do Actor
+    // Pelo fato de podermos alterar a mensagem com um comando
+    // precisamos manter a mensagem como um campo mutável dentro do actor
+    // Essa é a mensagem inicial (default)
     private String message = "Hello World!!!";
 
-    private HelloWorld(ActorContext<Command> context){
+    private HelloWorld(ActorContext<Command> context) {
         super(context);
     }
 
+
+    // Aqui é definida a lógica, o comportamento que é acionado
+    // ao receber uma mensagem
     @Override
     public Receive<Command> createReceive() {
-        return newReceiveBuilder()
-                .onMessageEquals(SayHello.INSTANCE, this::onSayHello)
+        // A definição do recebido é iniciada através de um builder
+        // onde é possível criar um método para cada tipo de mensagem recebida
+        return newReceiveBuilder() // então definimos um manipulador
+                .onMessageEquals(SayHello.INSTANCE, this::onSayHello) // como o primeiro metodo retorna a mensagem exata ao invés do tipo usamos onMessageEquals
                 .onMessage(ChangeMessage.class, this::onChangeMessage)
                 .build();
     }
@@ -57,11 +73,10 @@ public class HelloWorld extends AbstractBehavior<HelloWorld.Command>{
     private Behavior<Command> onChangeMessage(ChangeMessage command) {
         message = command.newMessage;
         return this;
-
     }
 
 
-    private Behavior<Command> onSayHello(){
+    private Behavior<Command> onSayHello() {
         System.out.println(message);
         return this;
     }
